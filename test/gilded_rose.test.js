9d632b34f022fa0ddf30a3217c097b9ffd629cdd
Fulldia <camille.lafourcade64@orange.fr>
2023-03-04 14:55:20 +0100

    update modif

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,8 +1,8 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const {Shop, Item, ShopDataAccess} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("foo", 0, 60)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
